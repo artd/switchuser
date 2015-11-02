3b492815ac76f320d15568a208549d67266299dd
artd <artd@artd.ch>
2015-11-02 23:06:08 +0700

    Switch User Plugin version 3.01 for Joomla 2.5 And Joomla 3

--- switchuser.php
+++ switchuser.php
@@ -11,6 +11,7 @@
 // no direct access
 defined( '_JEXEC' ) or die( 'Restricted access' );
 
+
 class plgSystemSwitchUser extends JPlugin
 {
 	function __construct(&$subject, $config = array())
@@ -23,15 +24,20 @@ class plgSystemSwitchUser extends JPlugin
 	{
 		$app =& JFactory::getApplication();
 		$option = JRequest::getCmd('option', '');
-		$view = JRequest::getCmd('view', $option == 'com_users' ? 'users' : '');
-		$layout = JRequest::getCmd('layout', 'default');
-		if (!$app->isAdmin() || $option != 'com_users' || $view != 'users' || $layout != 'default') {
+		$task = JRequest::getCmd('view', $option == 'com_users' ? 'view' : '');
+		
+		//if (!$app->isAdmin() || $option != 'com_users' || $task != 'view') {
+		if (!$app->isAdmin() || $option != 'com_users' || $task != 'users') {
 			return;
 		}
+
 		$document =& JFactory::getDocument();
 		$content = $document->getBuffer('component');
+
 		$pattern = '/name="cid\[\]" value="(\d+)"/';
+
 		if (!preg_match_all($pattern, $content, $matches)) {
+
 			return;
 		}
 		$userIds = $matches[1];
@@ -42,12 +48,17 @@ class plgSystemSwitchUser extends JPlugin
 		;
 		$db->setQuery($query);
 		$users = $db->loadObjectList('id');
+
+		
 		
 		$patterns = array();
 		$replacements = array();
 		foreach ($users as $userId => $user) {
-			$patterns[] = '|<a href="'.addcslashes(JRoute::_('index.php?option=com_users&task=user.edit&id='.(int)$userId), '?').'"[^>]+>\s*'.$user->name.'\s*</a>|';
-			$replacements[] = '${0} <a href="'.JURI::root().'index.php?switchuser=1&uid='.$userId.'" target="_blank" title="'.JText::sprintf('SWITCHUSER_FRONT_END', $user->username).'"><img style="margin: 0 10px;" src="'.JURI::root().'/plugins/system/switchuser/images/frontend-login.png" alt="'.JText::_('SWITCHUSER_FRONT_END').'" /></a>';
+			
+			//$patterns[] = '|<a href="'.addcslashes(JRoute::_('index.php?option=com_users&amp;view=user&amp;task=edit&amp;cid[]='.(int)$userId), '?').'"[^>]+>\s*'.$user->name.'\s*</a>|';
+			//$replacements[] = '${0} <a href="'.JURI::root().'index.php?switchuser=1&uid='.$userId.'" target="_blank" title="'.JText::sprintf('SWITCHUSER_FRONT_END', $user->username).'"><img style="margin: 0 10px;" src="'.JURI::root().'plugins/system/switchuser/switchuser/images/frontend-login.png" alt="'.JText::_('SWITCHUSER_FRONT_END').'" /></a>';
+			$patterns[] = '|<a href="'.addcslashes(JRoute::_('index.php?option=com_users&amp;task=user.edit&amp;id='.(int)$userId), '?').'"[^>]+>\s*'.$user->name.'\s*</a>|';
+			$replacements[] = '${0} <a href="'.JURI::root().'index.php?option=com_users&switchuser=1&uid='.$userId.'" target="_blank" title="'.JText::sprintf('SWITCHUSER_FRONT_END', $user->username).'"><img style="margin: 0 10px;" src="'.JURI::root().'plugins/system/switchuser/switchuser/images/frontend-login.png" alt="'.JText::_('SWITCHUSER_FRONT_END').'" /></a>';
 		}
 		$content = preg_replace($patterns, $replacements, $content);
 		$document->setBuffer($content, 'component');
@@ -72,7 +83,12 @@ class plgSystemSwitchUser extends JPlugin
 			return $app->redirect('index.php');
 		}
 		
-		$backendSessionId = JRequest::getVar(md5(JUtility::getHash('administrator')), null ,"COOKIE");
+		//$backendSessionId = JRequest::getVar(md5(JUtility::getHash('administrator')), null ,"COOKIE");
+
+		
+		$backendSessionId = JRequest::getVar(md5(JApplication::getHash('administrator')), null ,"COOKIE");
+
+
 		$query = 'SELECT userid'
 			. ' FROM #__session'
 			. ' WHERE session_id = '.$db->Quote($backendSessionId)
@@ -95,37 +111,60 @@ class plgSystemSwitchUser extends JPlugin
 
 		// If the user is blocked, redirect with an error
 		if ($instance->get('block') == 1) {
-			JError::raiseWarning('SOME_ERROR_CODE', JText::_('JERROR_NOLOGIN_BLOCKED'));
+			JError::raiseWarning('SOME_ERROR_CODE', JText::_('E_NOLOGIN_BLOCKED'));
 			$app->redirect('index.php');
 			return;
 		}
 
-		// Check the user can login.
-		$result	= $instance->authorise('core.login.site');
-		if (!$result) {
-			JError::raiseWarning(401, JText::_('JERROR_LOGIN_DENIED'));
-			return $app->redirect('index.php');
+		// Get an ACL object
+		$acl =& JFactory::getACL();
+
+		// Get the user group from the ACL
+		if ($instance->get('tmp_user') == 1) {
+			$grp = new JObject;
+			// This should be configurable at some point
+			$grp->set('name', 'Registered');
+		} else {
+			//$grp = $acl->getAroGroup($instance->get('id'));
 		}
 
-		// Mark the user as logged in
-		$instance->set('guest', 0);
+		//Authorise the user based on the group information
+		if(!isset($options['group'])) {
+			$options['group'] = 'USERS';
+		}
+
+		// if(!$acl->is_group_child_of( $grp->name, $options['group'])) {
+		// 	return JError::raiseWarning('SOME_ERROR_CODE', JText::_('E_NOLOGIN_ACCESS'));
+		// }
+
+		//Mark the user as logged in
+		$instance->set( 'guest', 0);
+		$instance->set('aid', 1);
+
+		// Fudge Authors, Editors, Publishers and Super Administrators into the special access group
+		// if ($acl->is_group_child_of($grp->name, 'Registered')      ||
+		//     $acl->is_group_child_of($grp->name, 'Public Backend'))    {
+		// 	$instance->set('aid', 2);
+		// }
+
+		//Set the usertype based on the ACL group name
+		$instance->set('usertype', $grp->name);
 
 		// Register the needed session variables
-		$session = JFactory::getSession();
+		$session =& JFactory::getSession();
 		$session->set('user', $instance);
-		
-		// Check to see the the session already exists.
-		$app->checkSession();
 
-		// Update the user related fields for the Joomla sessions table.
-		$db->setQuery(
-			'UPDATE `#__session`' .
-			' SET `guest` = '.$db->quote($instance->get('guest')).',' .
-			'	`username` = '.$db->quote($instance->get('username')).',' .
-			'	`userid` = '.(int) $instance->get('id') .
-			' WHERE `session_id` = '.$db->quote($session->getId())
-		);
-		$db->query();
+		// Get the session object
+		$table = & JTable::getInstance('session');
+		$table->load( $session->getId() );
+
+		$table->guest 		= $instance->get('guest');
+		$table->username 	= $instance->get('username');
+		$table->userid 		= intval($instance->get('id'));
+		$table->usertype 	= $instance->get('usertype');
+		//$table->gid 		= intval($instance->get('gid'));
+
+		$table->update();
 
 		// Hit the user last visit field
 		$instance->setLastVisit();
